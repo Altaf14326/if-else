d3c060e9db37f3e2dca4234c1447281173a743e3
Altaf14326 <altafsultani6@gmail.com>
2024-03-04 05:00:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+
+// function sum (math , chemistry ){
+// console.log (math + chemistry)
+
+// }
+// sum( 20+40)
+
+
+
+
+
+
+
+
+// function showString(){
+//     ('hello world')
+//     return 'altaf'
+// }
+// var myFunc = showString()
+// console.log(myFunc);
+
+
+
+
+// function percentageCal ( 
+//     mark1 + mark2 + mark3 + mark4
+// ){
+// return obtainMarks / total marks * 100
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector(".head");
+// console.log(head.innerHTML)
+
+// head.innerHTML = 'changed through js'
+
+
+
+var percentage= +prompt
+('enter your percentage!');
+ 
+if (percentage > 100) { 
+    console.log('bhai percent to dedy sahi!');
+}else if( percentage< 50){
+    console.log('malik raksha chalao!')
+}else if (percentage >= 60 && percentage >= 50){
+    console.log('c grade bna he maalik!')
+}
